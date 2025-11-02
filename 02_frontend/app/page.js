02b3bba3a80a74cc4f1364c2e8a0e5c8f7ae4013
Karn Yongsiriwit <karnyong@gmail.com>
2025-11-02 22:45:50 +0700

    front

--- 02_frontend/app/page.js
+++ 02_frontend/app/page.js
@@ -10,7 +10,7 @@ export default function Page() {
   useEffect(() => {
     async function getAttractions() {
       try {
-        const apiHost = process.env.NEXT_PUBLIC_API_HOST || "http://localhost:5000";
+        const apiHost = process.env.NEXT_PUBLIC_API_HOST;
         const res = await fetch(`${apiHost}/attractions`, { cache: "no-store" });
         if (!res.ok) throw new Error("Failed to fetch");
         const data = await res.json();
